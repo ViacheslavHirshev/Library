4cdc7313291326000660fb6035070d8c563be4b6
Viacheslav Hirshev <viacheslavgirshev@gmail.com>
2025-02-04 17:26:40 +0200

    More updates to styles, added js so book cards now appear

--- script.js
+++ script.js
@@ -14,13 +14,35 @@ function addBookToLibrary(title, authorName, pages)
 
 function displayBooks(library) 
 {
+    const booksContainer = document.querySelector(".books");
     for (let book of library)
     {
-        console.log(book);
+        createBookCard(book, booksContainer);
     }
 }
 
-addBookToLibrary("123", "132", "321");
-addBookToLibrary("123", "132", "321");
-addBookToLibrary("123", "132", "321");
+function createBookCard(book, booksContainer) 
+{
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    let bookTitle = document.createElement("div");
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add("book-title");
+    bookCard.append(bookTitle);
+
+    let atrName = document.createElement("div");
+    atrName.textContent = book.authorName;
+    bookCard.append(atrName);
+
+    let pages = document.createElement("div");
+    pages.textContent = "p. - " + book.pages;
+    bookCard.append(pages);
+
+    booksContainer.append(bookCard);
+}
+
+addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
+addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
+addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
 displayBooks(bookLibrary);
