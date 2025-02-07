7d9d22bcf026e98213cf51add42cbce4444c8ad3
Viacheslav Hirshev <viacheslavgirshev@gmail.com>
2025-02-07 21:26:40 +0200

    Now book cards can be removed and read/not read state can be toggled

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let bookLibrary = [];
+const bookLibrary = [];
 
 function Book(title, authorName, pages)
 {
     this.title = title;
     this.authorName = authorName;
     this.pages = pages;
+    this.read = false;
 }
 
 function addBookToLibrary(title, authorName, pages)
@@ -14,8 +15,9 @@ function addBookToLibrary(title, authorName, pages)
 
 function displayBooks(library) 
 {
-    const booksContainer = document.querySelector(".books");
     clearBooks();
+
+    const booksContainer = document.querySelector(".books");
     for (let book of library)
     {
         createBookCard(book, booksContainer);
@@ -30,21 +32,53 @@ function clearBooks()
 
 function createBookCard(book, booksContainer) 
 {
-    let bookCard = document.createElement("div");
+    const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
 
-    let bookTitle = document.createElement("div");
+    const bookTitle = document.createElement("div");
     bookTitle.textContent = book.title;
     bookTitle.classList.add("book-title");
     bookCard.append(bookTitle);
 
-    let atrName = document.createElement("div");
-    atrName.textContent = book.authorName;
-    bookCard.append(atrName);
+    const authorName = document.createElement("div");
+    authorName.textContent = book.authorName;
+    bookCard.append(authorName);
 
-    let pages = document.createElement("div");
+    const pages = document.createElement("div");
     pages.textContent = "p. - " + book.pages;
     bookCard.append(pages);
+
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btn-container");
+    bookCard.append(btnContainer);
+
+    const isReadBtn = document.createElement("button");
+    isReadBtn.textContent = "Not read";
+    isReadBtn.classList.add("not-read-btn");
+    isReadBtn.addEventListener("click", () =>
+    {
+        if (toggleRead(book))
+        {
+           isReadBtn.textContent = "Read";
+        }
+        else
+        {
+            isReadBtn.textContent = "Not read";
+        }
+        isReadBtn.classList.toggle("not-read-btn");
+        isReadBtn.classList.toggle("read-btn");
+    });
+    btnContainer.append(isReadBtn);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.classList.add("remove-btn");
+    removeBtn.addEventListener("click", () =>
+    {
+        bookLibrary.splice(bookLibrary.indexOf(book), 1);
+        displayBooks(bookLibrary);
+    })
+    btnContainer.append(removeBtn);
 
     booksContainer.append(bookCard);
 }
@@ -81,14 +115,21 @@ function submitForm()
             const pages = document.querySelector("#pages-field").value;
 
             addBookToLibrary(title, author, pages);
+            document.querySelector("dialog").close();
             displayBooks(bookLibrary);
         }
     );
 }
 
+function toggleRead(book)
+{
+    book.read = !book.read;
+    return book.read;
+}
+
 addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
-addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
-addBookToLibrary("\"All quiet on the western front\"", "Erih Maria Remark", "321");
+addBookToLibrary("\"All quiet on the western \"", "Erih Maria Remark", "321");
+addBookToLibrary("\"All quiet on the \"", "Erih Maria Remark", "321");
 displayBooks(bookLibrary);
 bindAddBtn();
 bindCloseBtn();
